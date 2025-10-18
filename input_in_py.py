b6246fa73f705e1211600beb9a6ee5c543011d7a
jatinrao7458 <jatinrao7458@gmail.com>
2025-10-18 11:30:35 +0530

    adding how to input in phython

---
+++ input_in_py.py
@@ -0,0 +1,8 @@
+marks = list(map(int,input().split()))
+# work of map is to convert string to int 
+# split help to store value in difff variable 
+print(marks[0])
+# to get the length of array in phyton
+print(len(marks))
+for i in range (0,len(marks),1):
+    print(marks[i])
