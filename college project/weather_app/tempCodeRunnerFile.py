704f729d18b5060ae673ee51242668c39f893349
unknown <jatinrao7458>
2025-12-30 13:46:51 +0530

    adding code for making a weather app

---
+++ college project/weather_app/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+w_label=Label(win,text="Weather climate",ont= ("Arial",20))
+# w_label.place(x=25, y=260, height=50,width=210)
+
+# wb_label=Label(win,text="Weather Discription",ont= ("Arial",20))
+# wb_label.place(x=25, y=330, height=50,width=210)
+
+# temp_label=Label(win,text="Temperature",ont= ("Arial",20))
+# temp_label.place(x=25, y=400, height=50,width=210)
+
+# per_label=Label(win,text="Pressure",ont= ("Arial",20))
+# per_label.place(x=25, y=470, height=50,width=210)
